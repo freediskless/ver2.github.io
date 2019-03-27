14e8db476b8783542e95918bd7251ff0706de9e5
freediskless <48971078+freediskless@users.noreply.github.com>
2019-03-27 08:56:35 +0700

    Add files via upload

---
+++ license.php
@@ -0,0 +1,17 @@
+<license>
+	<result value="OK" />
+	<username value="Free CCBoot" />
+	<licenseemail value="krocolatoz@myself.com" />
+	<expiretime value="2999/12/31 23:59:59" />
+</license>
+<urls>
+	<homepageurl value="http://tools-warnet.blogspot.com" />
+	<helpurl value="http://tools-warnet.blogspot.com" />
+	<userforumurl value="http://tools-warnet.blogspot.com" />
+	<facebookurl value="https://www.facebook.com/bdstd21" />
+	<licensemanager value="http://tools-warnet.blogspot.com" />
+	<supporturl value="https://www.facebook.com/bdstd21" />
+	<bugreportsurl value="http://tools-warnet.blogspot.com" />
+	<signupurl value="http://tools-warnet.blogspot.com" />
+	<forgetpwdurl value="http://tools-warnet.blogspot.com" />
+</urls>
